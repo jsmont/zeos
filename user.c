c168484f2638c0bda2de8571b27319b1eb677162
Jordi Solà Montserrat <jsmont.sol@gmail.com>
2014-12-12 18:37:43 +0100

    Bug fixes

--- user.c
+++ user.c
@@ -9,8 +9,8 @@ main(void) {
 /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
 /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
 
-    //runjp();
-    runjp_rank(1,5);
+    runjp();
+    //runjp_rank(1,5);
     //runjp_rank(7,11);
 
     while(1) {
